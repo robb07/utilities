4560bb0c910e23a59b69657e9ab16c783640068c
robb07 <zap376@gmail.com>
2014-06-15 13:47:01 -0400

    Added current directory support for no arguments given

--- src/du.py
+++ src/du.py
@@ -13,7 +13,7 @@ import re
 
 from argparse import ArgumentParser
 
-DEBUG = 1
+DEBUG = 0
 TEST = 0
 
 possible_units = ['K', 'M', 'G', 'T', 'P', 'E', 'Z', 'Y'] 
@@ -66,9 +66,10 @@ def main():
     
     # Setup argument parser
     parser = ArgumentParser(description='Summarize the disk utilization of a file, recursively for a directory')
-    parser.add_argument('FILE',help='Summarize the disk utilization of each file or directory in FILE')
-    parser.add_argument('-a','--all',help='Print for files too',action='store_true')
-    parser.add_argument('-s','--summarize',help='Only print total disk utilization for each entry in FILE',action='store_true')
+    parser.add_argument('FILE',help='Summarize the disk utilization of each file or directory in FILE', nargs='?')
+    parser.add_argument('-a','--all',help='Print size for files too',action='store_true')
+    parser.add_argument('-s','--summarize',help='Only print total size for each entry in FILE',action='store_true')
+    parser.add_argument('-t','--total',help='Print total combined size of entries in FILE',action='store_true')
     parser.add_argument('-d','--max-depth',help='Print disk utilization for directories no deeper than MAX_DEPTH',type=int)
     parser.add_argument('-u','--units',help='Print display utilization in specified UNITS (K, M, G, T, P, E, Z, Y are powers of 1024, KB, MB, ... are powers of 1000, * and *B are dynamic)')
     
@@ -77,15 +78,19 @@ def main():
     args = parser.parse_args()
     
     # Unpack the arguments
-    files = [f.strip('"') for f in re.findall(r'[^\s"]+|"[^"]*"', args.FILE)]
-    # explanation of the regular expression:
-    # [^\s"]+ -- matches all characters until a white space or quote (i.e. strings separated by white space or quotes)
-    # |       -- or
-    # "[^"]*" -- matches quote plus all characters through the next quote (i.e. only matches an object enclosed in quotes)
-    # the or keeps the regular expression match going through white space enclosed in quotes
+    if args.FILE:
+        files = [f.strip('"') for f in re.findall(r'[^\s"]+|"[^"]*"', args.FILE)]
+        # explanation of the regular expression:
+        # [^\s"]+ -- matches all characters until a white space or quote (i.e. strings separated by white space or quotes)
+        # |       -- or
+        # "[^"]*" -- matches quote plus all characters through the next quote (i.e. only matches an object enclosed in quotes)
+        # the or keeps the regular expression match going through white space enclosed in quotes
+    else:
+        files = [os.curdir]
     
     print_files = args.all
     summarize = args.summarize
+    total = args.total
     max_depth = args.max_depth
     units = args.units
     
@@ -100,8 +105,9 @@ def main():
     if summarize:
         max_depth = 0
         
-    for file1 in files:
-        du(file1, 0, max_depth, print_files, units)
+    sizes = [du(file1, 0, max_depth, print_files, units) for file1 in files]
+    if total:
+        print disp_units(sum(sizes),units), '\tTotal'
     
     return 0
     
@@ -125,9 +131,11 @@ if __name__ == '__main__':
         sys.argv.append('C:\Python27')
     elif TEST == 5:
         sys.argv.append('-s')
+        sys.argv.append('-t')
         sys.argv.append('-d 3')
         sys.argv.append('-u *')
         sys.argv.append('C:\Python27\lib C:\Python27\Tools    "C:\Program Files\Java"')
+    
         
     sys.exit(main())
 
